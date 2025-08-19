3acd7fb1e10a5b1e9e93ddc551ff8924cad3683d
Teddytedmic <60871282+Teddytedmic@users.noreply.github.com>
2025-08-19 08:54:26 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,7 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~pr9wmTYL#YPCrsQJO52JrqB1N2FKx-SDU9KFpHSUOT3z0E7wPgZw",  // add your Session Id here
-  
+  SESSION_ID: "malvin~pr9wmTYL#YPCrsQJO52JrqB1N2FKx-SDU9KFpHSUOT3z0E7wPgZw", || "malvin~8yESBALZ#wv9CQ1BUOm5dSWP8FGH59F2lW_d3VmYyc9SekVGGTSk"
   OWNER_NUMBER: "263713012347", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
